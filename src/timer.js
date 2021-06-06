82f7c4890e33b3cf0ea70e59d83c6440ca32114d
Van Eyck Jo <jo.vaneyck@gmail.com>
2021-06-06 11:28:36 +0200

    format

--- src/timer.js
+++ src/timer.js
@@ -1,19 +1,19 @@
 window.onload = function () {
-	$('#start').click(function(){
+	$('#start').click(function () {
 		var display = $('#time');
 		var duration = getDesiredDurationInMinutes() * 60;
 		startTimer(duration, display);
 	});
 
-	$('#stop').click(function(){
+	$('#stop').click(function () {
 		stopTimer();
 		stopAlarm();
 	});
 };
 
-function getDesiredDurationInMinutes(){
+function getDesiredDurationInMinutes() {
 	var durationInMinutes = $("#duration").val();
-	if(! durationInMinutes){
+	if (!durationInMinutes) {
 		durationInMinutes = $('#duration').attr('placeholder');
 	}
 
@@ -25,38 +25,38 @@ var timerId;
 function startTimer(duration, display) {
 	stopTimer();
 
-    var timer = duration;
-    var minutes;
-    var seconds;
-    
-    timerId = setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
+	var timer = duration;
+	var minutes;
+	var seconds;
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+	timerId = setInterval(function () {
+		minutes = parseInt(timer / 60, 10);
+		seconds = parseInt(timer % 60, 10);
 
-        $(display).text(minutes + ":" + seconds);
+		minutes = minutes < 10 ? "0" + minutes : minutes;
+		seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        if (--timer < 0) {
-        	clearInterval(timerId);
-            triggerAlarm();
-        }
-    }, 1000);
+		$(display).text(minutes + ":" + seconds);
+
+		if (--timer < 0) {
+			clearInterval(timerId);
+			triggerAlarm();
+		}
+	}, 1000);
 }
 
-function stopTimer(){
+function stopTimer() {
 	clearInterval(timerId);
 }
 
 var audio = document.getElementById("delete_your_code_audio");
 
-function triggerAlarm(){
+function triggerAlarm() {
 	audio.loop = true;
 	audio.currentTime = 0;
 	audio.play();
 }
 
-function stopAlarm(){
+function stopAlarm() {
 	audio.pause();
 }
